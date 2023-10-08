e217fbbd33f1ff80079164e62552ff810f898168
Sudeep Maharjan <sudeep.maharjan@science.org.au>
2023-10-08 20:11:24 +1100

    First commit

---
+++ tic-tac-tow/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic tac toe</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ tic-tac-tow/src/components/Square.js
@@ -0,0 +1,6 @@
+import {useState} from 'react';
+
+const Square = ({value, onSquareClick}) => {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+export default Square;
